87ff4ae39a5947f007ef00e22dd72aaa6a5f4b6e
Mike Bryant <mikesname@gmail.com>
2011-12-13 12:16:30 +0000

    Merge EHRI theme changes into 1.2

--- plugins/sfEhriThemePlugin/config/sfEhriThemePluginConfiguration.class.php
+++ plugins/sfEhriThemePlugin/config/sfEhriThemePluginConfiguration.class.php
@@ -4,8 +4,8 @@
  * This file is part of Qubit Toolkit.
  *
  * Qubit Toolkit is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * Qubit Toolkit is distributed in the hope that it will be useful,
@@ -20,19 +20,15 @@
 class sfEhriThemePluginConfiguration extends sfPluginConfiguration
 {
   public static
-    $summary = 'Theme plugin.  Adaptation of Classic theme that uses drop-down menus and mixes fixed and fluid width elements.  Further adapted for EHRI colours.',
+    $summary = 'Theme plugin.  Adaptation of Classic theme that uses drop-down menus and mixes fixed and fluid width elements.',
     $version = '1.0.0';
 
   public function contextLoadFactories(sfEvent $event)
   {
     $context = $event->getSubject();
 
-    $context->response->addStylesheet('/plugins/sfClassicPlugin/css/main', 'last', array('media' => 'all'));
+    $context->response->addStylesheet('/css/classic', 'last', array('media' => 'all'));
     $context->response->addStylesheet('/plugins/sfEhriThemePlugin/css/style', 'last', array('media' => 'all'));
-
-    $context->response->addStylesheet('/plugins/sfEhriThemePlugin/css/print', 'last', array('media' => 'print'));
-
-    $context->response->addStylesheet('/plugins/sfEhriThemePlugin/css/print-ie', 'last', array('condition' => 'IE', 'media' => 'print'));
 
     $context->response->addJavaScript('/plugins/sfEhriThemePlugin/js/navigation', 'last');
   }
