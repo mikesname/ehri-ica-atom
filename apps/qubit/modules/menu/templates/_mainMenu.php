cf14513800c7c8281c519800797717903eb1c9f7
david <david@c41d51f2-d139-0410-bf3a-ab6474c13fa3>
2012-06-21 23:49:27 +0000

    Merge r11788 and r11789 (fixes issue 2266) from qubit trunk
    git-svn-id: http://qubit-toolkit.googlecode.com/svn/branches/ica-atom@11791 c41d51f2-d139-0410-bf3a-ab6474c13fa3

--- apps/qubit/modules/menu/templates/_mainMenu.php
+++ apps/qubit/modules/menu/templates/_mainMenu.php
@@ -3,7 +3,13 @@
   <h2 class="element-invisible"><?php echo __('Main menu') ?></h2>
 
   <div class="content">
-    <?php echo QubitMenu::displayHierarchyAsList($mainMenu, 0, array('overrideVisibility' => array('admin' => $sf_user->hasCredential('administrator')))) ?>
+    <?php // Using $sf_user->hasGroup() since it relies on database,
+          // $sf_user->hasCredential('administrator') relies on session storage
+          // This adds more db access but we are caching anyways
+          // See also issue 2266
+          $isAdministrator = $sf_user->hasGroup(QubitAclGroup::ADMINISTRATOR_ID) ?>
+    <?php echo QubitMenu::displayHierarchyAsList($mainMenu, 0, array('overrideVisibility' => array('admin' => $isAdministrator))) ?>
   </div>
+
 
 </div>
